858fac5ef77142e59b5ee6074d823ccd9761fc06
Greg Lamp <lamp.greg@gmail.com>
2013-09-18 16:42:51 -0400

    removed debugging

--- src/main.py
+++ src/main.py
@@ -34,6 +34,3 @@ if __name__=="__main__":
         sys.stdout.write(json.dumps(data) + "\n")
         sys.stdout.flush()
 
-        with open("log.txt", "wb") as f:
-            f.write(json.dumps(data) + "\n")
- 
