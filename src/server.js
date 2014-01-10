30e5ca916e18f78df9f1b615788973d0051c88e9
Greg <greg@yhatHQ.com>
2014-01-10 11:36:00 -0500

    adding in screenshot

--- src/server.js
+++ src/server.js
@@ -19,14 +19,14 @@ module.exports = function(lang) {
 
   // all environments
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
+  app.set('views', path.join(__dirname, '../views'));
   app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, '../public')));
 
   // development only
   if ('development' == app.get('env')) {
@@ -85,6 +85,10 @@ module.exports = function(lang) {
 
   app.get('/', function(req, res) {
     res.send({ status: "up" });
+  });
+
+  app.get('/demo', function(req, res) {
+    res.render("demo");
   });
 
   app.post('/', function(req, res) {
